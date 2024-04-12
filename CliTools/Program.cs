4b85df3b8e340d9b43c18f748ff4f97dbe7db2a8
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-04-12 10:03:00 +0200

    Add weather

--- CliTools/Program.cs
+++ CliTools/Program.cs
@@ -1,1 +1,15 @@
 ﻿using Cocona;
+
+//CoconaApp.Run(([Argument(Description = "First Name")] string name, 
+//               [Option(Description = "Last name")] string? lastName) =>
+//{
+//    Console.WriteLine($"Hello {name}");
+//});
+
+var builder = CoconaApp.CreateBuilder();
+
+
+
+var app =  builder.Build();
+
+app.Run();
