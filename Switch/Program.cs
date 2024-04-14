33157371a0f716310ca54d9c7b150a64f06f5174
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-04-14 08:33:39 +0200

    Add switch for IL testing

---
+++ Switch/Program.cs
@@ -0,0 +1,35 @@
+﻿var number = Random.Shared.Next(1, 11);
+
+switch(number)
+{
+    case 1:
+        Console.WriteLine("Value was 1");
+        break;
+    case 2:
+        Console.WriteLine("Value was 2");
+        break;
+    case 3:
+        Console.WriteLine("Value was 3");
+        break;
+    case 4:
+        Console.WriteLine("Value was 4");
+        break;
+    case 5:
+        Console.WriteLine("Value was 5");
+        break;
+    case 6:
+        Console.WriteLine("Value was 6");
+        break;
+    case 7:
+        Console.WriteLine("Value was 7");
+        break;
+    case 8:
+        Console.WriteLine("Value was 8");
+        break;
+    case 9:
+        Console.WriteLine("Value was 9");
+        break;
+    case 10:
+        Console.WriteLine("Value was 10");
+        break;
+}
